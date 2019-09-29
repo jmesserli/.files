9bbb4f05f3d2040ca4e59b2b13f95476307b0f25
Joel Messerli <hi.github@peg.nu>
2019-09-29 12:59:15 +0200

    Create setup.sh

---
+++ config/nvim/setup.sh
@@ -0,0 +1,4 @@
+# Ensure curl and git are installed
+
+curl -sSLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/jmesserli/.files/master/config/nvim/init.vim
+curl -sSLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
