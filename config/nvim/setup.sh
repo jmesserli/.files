644c8570bb10c53cd97d46746e5e744fc6227c47
Joel Messerli <hi.github@peg.nu>
2019-09-29 13:00:41 +0200

    Update setup.sh

--- config/nvim/setup.sh
+++ config/nvim/setup.sh
@@ -2,3 +2,5 @@
 
 curl -sSLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/jmesserli/.files/master/config/nvim/init.vim
 curl -sSLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# or: curl -sSL https://peg.nu/nvim_setup | bash
