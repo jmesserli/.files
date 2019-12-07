a11ebe1f57541304d3ae699f5388d802d141404a
Joel Messerli <hi.github@peg.nu>
2019-12-07 23:24:38 +0100

    Update curl-alias-killer.ps1

--- snippets/powershell/curl-alias-killer.ps1
+++ snippets/powershell/curl-alias-killer.ps1
@@ -3,4 +3,6 @@ if (!$profileExists) {
     Write-Output "Creating profile $profile"
     New-Item -Force -ItemType File $profile
 }
-Add-Content -Path $profile "Remove-Item Alias:\curl`nRemove-Item Alias:\wget"
+Add-Content -Path $profile "`nRemove-Item Alias:\curl`nRemove-Item Alias:\wget"
+
+# or run: iwr -useb https://peg.nu/pscurlr | iex
