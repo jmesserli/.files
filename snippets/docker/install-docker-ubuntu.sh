627807afe35666c6dbed4c61e761e847fbaf2dfa
Joel Messerli <hi.github@peg.nu>
2021-02-02 17:45:59 +0100

    update docker compose version

--- snippets/docker/install-docker-ubuntu.sh
+++ snippets/docker/install-docker-ubuntu.sh
@@ -21,7 +21,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
     sudo apt update &&
     sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" \
     -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
